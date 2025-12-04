8fa58406f8c0b3eb40a8ddc77058ddfdb2b7a306
Nicola Romanò <nicola.romano@ed.ac.uk>
2025-12-04 23:35:53 +0000

    Refactor invalid ID check into a separate function

--- Day2/day2.py
+++ Day2/day2.py
@@ -9,14 +9,22 @@ input_data = input_data.split(',')
 
 ranges = [(int(r[0]), int(r[1])) for r in (x.split('-') for x in input_data)]
 
+# Part 1 - Sum of all invalid IDs made by repeating the same digits twice
+
+def check_invalid_id(n:int) -> bool:
+    n_str = str(n)
+    if len(n_str) % 2 != 0: # odd length can't be invalid
+        return False
+    half = len(n_str) // 2
+    return n_str[:half] == n_str[half:]
+
 sum = 0
 
 for r in ranges:
     for n in range(r[0], r[1]+1):
-        if len(str(n)) % 2 == 0:
-            first_half = str(n)[:len(str(n))//2]
-            second_half = str(n)[len(str(n))//2:]
-            if first_half == second_half:
-                sum += n
+        if check_invalid_id(n):
+            sum += n
                 
 print(f"The sum of all invalid IDs is {sum}")
+
+# Part 2
