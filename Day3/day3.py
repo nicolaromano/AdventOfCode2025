dab976c796ebed363848f039acaefe019dd2ed25
Nicola Romanò <nicola.romano@ed.ac.uk>
2025-12-06 07:34:24 +0000

    Part2

--- Day3/day3.py
+++ Day3/day3.py
@@ -40,3 +40,41 @@ assert (get_largest_joltage("818181911112111") == 92)
 total_joltage = sum(get_largest_joltage(v) for v in input_data)
 
 print(f"The total largest joltage is {total_joltage}")
+
+# Part 2 - Now we can turn on 12 batteries
+
+def get_largest_joltage_12(num: str, debug:bool = False) -> str:
+    """
+    Gets the largest "joltage" from a series of batteries, given you can
+    turn on 12
+
+    Parameters:
+    num (str): A string of digits representing battery joltages
+    Returns:
+    int: The largest possible joltage from turning on 12 batteries.
+    """
+
+    vals = [int(x) for x in num]
+    jottage = ""
+
+    for n in range(12):
+        # Find the largest digit excluding the last n
+        if debug:
+            print(f"Checking max of {vals[:len(vals) - 12 + n + 1]}")
+
+        max_val = max(vals[:len(vals)-12+n+1])
+        max_pos = vals.index(max_val)
+        jottage += str(max_val)
+        vals = vals[max_pos+1:]
+        if debug:
+            print(f"{n=} {max_val=} {max_pos=} {jottage=}")
+
+    return jottage
+
+assert (get_largest_joltage_12("987654321111111") == "987654321111")
+assert (get_largest_joltage_12("811111111111119") == "811111111119")
+assert (get_largest_joltage_12("234234234234278") == "434234234278")
+assert (get_largest_joltage_12("818181911112111") == "888911112111")
+
+total_joltage_12 = sum(int(get_largest_joltage_12(v)) for v in input_data)
+print(f"The total largest joltage from 12 batteries is {total_joltage_12}")
