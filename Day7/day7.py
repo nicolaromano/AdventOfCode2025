dadd0ffd3447e0e28b6260b18d0ee7cf14b584c9
Nicola Romanò <nicola.romano@ed.ac.uk>
2025-12-21 22:25:59 +0000

    Part 2

--- Day7/day7.py
+++ Day7/day7.py
@@ -1,9 +1,9 @@
+from functools import cache
+
 input_file = "input.txt"
 
 with open(input_file, 'r') as file:
     values = file.readlines()
-
-print(values)
 
 test_data = [".......S.......",
 "...............",
@@ -29,7 +29,7 @@ def count_splits(input:list[str]) -> int:
     beam_pos = {(input[0].find("S"), 0)}
     num_splits = 0
 
-    for row in range(1, len(input)):                   
+    for row in range(1, len(input)):
         for col, val in enumerate(input[row]):
             if val == ".":
                 for beam in list(beam_pos):
@@ -48,3 +48,25 @@ assert(count_splits(test_data) == 21)
 
 print(f"Part 1 - The number of splits is {count_splits(values)}")
 
+# Part 2 - Splitters randomly move the beams left or right, so we need to track all possible beam positions
+# This is essentially a DFS through the grid
+
+@cache # Memoization to avoid recalculating paths from the same position
+# Note: we pass input as a tuple because lists are not hashable, and we can't @cache them
+def count_paths(input:tuple[str], beam_pos_x:int, beam_pos_y:int) -> int:
+    if beam_pos_y >= len(input) -1:
+        return 1
+    
+    if input[beam_pos_y][beam_pos_x] == ".": # Empty space, beam continues straight
+        return count_paths(input, beam_pos_x, beam_pos_y+1)
+    elif input[beam_pos_y][beam_pos_x] == "^": # Splitter, beam splits left and right
+        return (count_paths(input, beam_pos_x-1, beam_pos_y+1) +
+                count_paths(input, beam_pos_x+1, beam_pos_y+1))
+    else: # Invalid position (outside the grid)
+        return 0
+    
+bpx, bpy = test_data[0].find("S"), 0
+assert(count_paths(tuple(test_data[1:]), bpx, bpy) == 40)
+
+bpx, bpy = values[0].find('S'), 0
+print(f"Part 2 - The number of possible paths is {count_paths(tuple(values[1:]), bpx, bpy)}")
